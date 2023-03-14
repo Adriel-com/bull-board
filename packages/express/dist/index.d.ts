2f57e7ef6d85acb945ed0641f47434cd01c46088
Glenn <glenn.heo@adriel.com>
2023-03-14 10:39:09 +0900

    modify the dependency

--- packages/express/dist/index.d.ts
+++ packages/express/dist/index.d.ts
@@ -1,5 +1,1 @@
-export { createBullBoard } from '@bull-board/api';
-export { BullAdapter } from '@bull-board/api/bullAdapter';
-export { BullMQAdapter } from '@bull-board/api/bullMQAdapter';
-export { BullMQProAdapter } from '@bull-board/api/bullMQProAdapter';
 export { ExpressAdapter } from './ExpressAdapter';
--- packages/hapi/dist/index.js
+++ packages/hapi/dist/index.js
@@ -1,12 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.HapiAdapter = exports.BullMQAdapter = exports.BullAdapter = exports.createBullBoard = void 0;
-var api_1 = require("@bull-board/api");
-Object.defineProperty(exports, "createBullBoard", { enumerable: true, get: function () { return api_1.createBullBoard; } });
-var bullAdapter_1 = require("@bull-board/api/bullAdapter");
-Object.defineProperty(exports, "BullAdapter", { enumerable: true, get: function () { return bullAdapter_1.BullAdapter; } });
-var bullMQAdapter_1 = require("@bull-board/api/bullMQAdapter");
-Object.defineProperty(exports, "BullMQAdapter", { enumerable: true, get: function () { return bullMQAdapter_1.BullMQAdapter; } });
+exports.HapiAdapter = void 0;
 var HapiAdapter_1 = require("./HapiAdapter");
 Object.defineProperty(exports, "HapiAdapter", { enumerable: true, get: function () { return HapiAdapter_1.HapiAdapter; } });
 //# sourceMappingURL=index.js.map
